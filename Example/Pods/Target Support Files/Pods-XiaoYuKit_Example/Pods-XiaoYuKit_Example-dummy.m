476f2286031459a21d72f905cf2dd3595dcb4c72
RJ <841348093@qq.com>
2018-08-25 10:09:54 +0800

    0.1.0

---
+++ Example/Pods/Target Support Files/Pods-XiaoYuKit_Example/Pods-XiaoYuKit_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_XiaoYuKit_Example : NSObject
+@end
+@implementation PodsDummy_Pods_XiaoYuKit_Example
+@end
---
+++ XiaoYuKit/Classes/BaseViewController/RJBaseViewController.swift
@@ -0,0 +1,95 @@
+//
+//  RJBaseViewController.swift
+//  swiftTest
+//
+//  Created by RJ on 2018/7/16.
+//  Copyright © 2018年 RJ. All rights reserved.
+//
+
+import UIKit
+
+let VCBackGorundColor = UIColor(patternImage: UIImage(named: "sport_background")!)
+
+
+enum RJNavBarItemLocaltion :Int {
+    case left_first
+    case left_second
+    case right_first
+    case right_second
+}
+
+class RJBaseViewController: UIViewController {
+    var navBarHidden:Bool {
+        get{
+            return navigationController?.navigationBar.isHidden ?? false
+        }
+        set(navBarHidden){
+            navigationController?.navigationBar.isHidden = navBarHidden
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addNavBarItemBtn(.left_first, nil, "nav_back", self, #selector(exitViewController), .touchUpInside)
+        configureNavigation()
+        view.backgroundColor = VCBackGorundColor
+    }
+    func addSubview(_ subview:UIView) -> Void {
+        view.addSubview(subview);
+    }
+    
+    func configureNavigation() {
+        
+    }
+    
+    func navBarTitle(_ title:String? ,_ color:UIColor = .white , _ font:UIFont = RJFontMedium(16)) -> Void {
+        guard let _ = self.navigationController else { return  }
+        let lable = UILabel()
+        lable.text = title
+        lable.textColor = color
+        lable.font      = font
+        
+        navigationItem.titleView = lable
+    }
+    func addNavBarItemBtn(_ localtion:RJNavBarItemLocaltion,_ title:String?,_ image:String?,_ target:Any?,_ action:Selector, _ controlEvents:UIControlEvents) -> Void {
+        guard let _ = self.navigationController else { return  }
+        let btn = UIButton(type: .custom)
+        btn.addTarget(target, action: action, for: controlEvents)
+        if title?.count != nil {
+            btn.setTitle(title!, for: .normal)
+        }else{
+            if image?.count != nil {
+                btn.setImage(UIImage(named: image!), for: .normal)
+            }
+        }
+        btn.sizeToFit()
+        let item = UIBarButtonItem(customView: btn)
+        //        navigationItem.rightBarButtonItem = item;
+        switch localtion {
+        case .left_first:
+            var leftBarButtonItems = navigationItem.leftBarButtonItems != nil ? navigationItem.leftBarButtonItems! : [UIBarButtonItem]()
+            leftBarButtonItems.insert(item, at: 0)
+            navigationItem.leftBarButtonItems = leftBarButtonItems
+        case .left_second:
+            var leftBarButtonItems = navigationItem.leftBarButtonItems != nil ? navigationItem.leftBarButtonItems! : [UIBarButtonItem]()
+            leftBarButtonItems.insert(item, at: 1)
+            navigationItem.leftBarButtonItems = leftBarButtonItems
+        case .right_first:
+            var rightBarButtonItems = navigationItem.rightBarButtonItems != nil ? navigationItem.rightBarButtonItems! : [UIBarButtonItem]()
+            rightBarButtonItems.insert(item, at: 0)
+            navigationItem.rightBarButtonItems = rightBarButtonItems
+        case .right_second:
+            var rightBarButtonItems = navigationItem.rightBarButtonItems != nil ? navigationItem.rightBarButtonItems! : [UIBarButtonItem]()
+            rightBarButtonItems.insert(item, at: 1)
+            navigationItem.rightBarButtonItems = rightBarButtonItems
+        }
+    }
+    @objc
+    func exitViewController() -> Void {
+        if self.navigationController != nil {
+            navigationController?.popViewController(animated: true)
+        }else{
+            dismiss(animated: true, completion: nil)
+        }
+    }
+}
